c96d45a033c5fa1e1ba7645b1b0f86733018aed5
Reyhaneh <reyhaneh.gh.davodi@gmail.com>
2023-08-20 16:50:16 +0330

    sending email

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -3,8 +3,14 @@ from send_email import send_email
 st.title("Contact Me")
 with st.form("email_form"):
     user_email=st.text_input("your email address")
-    message=st.text_area("your message")+"\n"+user_email
+    message=st.text_area("your message")
+    final_message=f"""\
+Subject: new message from {user_email}
+    
+From: {user_email}
+{message} 
+    """
     sub_btn=st.form_submit_button("submit")
     if sub_btn:
-        send_email(message)
-        st.write("your message sended!")
+        send_email(final_message)
+        st.info("your message sended!")
